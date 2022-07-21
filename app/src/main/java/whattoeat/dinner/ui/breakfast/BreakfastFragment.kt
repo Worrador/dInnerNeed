b809e8e0b5e2082b7b51ddca73cf3aaecfa49cab
Akos Poleczki <a.poleczki@gmail.com>
2022-07-21 18:32:51 +0200

    multiple choice list added to home fragment

--- app/src/main/java/whattoeat/dinner/ui/breakfast/BreakfastFragment.kt
+++ app/src/main/java/whattoeat/dinner/ui/breakfast/BreakfastFragment.kt
@@ -1,21 +1,35 @@
 package whattoeat.dinner.ui.breakfast
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import whattoeat.dinner.databinding.FragmentHomeBinding
 
-class BreakfastFragment : Fragment() {
+
+class BreakfastFragment : Fragment(), AdapterView.OnItemClickListener {
 
     private var _binding: FragmentHomeBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+    private fun setMultipleListView(): ArrayList<String>{
+        val arrayList: ArrayList<String> = ArrayList()
+
+        for(i in 1..10){
+            arrayList.add("App list  :D $i")
+        }
+        return arrayList
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,15 +42,28 @@ class BreakfastFragment : Fragment() {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        breakfastViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val listView: ListView = binding.breakfastListView
+        val listOfItem: ArrayList<String> = setMultipleListView()
+        getContext()?.let { val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(it, android.R.layout.simple_list_item_multiple_choice, listOfItem)
+            listView.adapter = arrayAdapter
         }
+
+        listView.choiceMode = ListView.CHOICE_MODE_MULTIPLE
+
+        listView.onItemClickListener = this
+
         return root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val options: String = parent?.getItemAtPosition(position) as String
+        if (view != null) {
+            Toast.makeText(view.getContext(), "Clicked By: $options", Toast.LENGTH_LONG).show()
+        }
     }
 }
